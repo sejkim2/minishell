96dde3df47d975c772bb69531eb3e85cd31e7446
J-millar99 <wlwogus33@gmail.com>
2023-10-11 13:52:59 +0900

    13:52

--- srcs/utils_dir/exec.c
+++ srcs/utils_dir/exec.c
@@ -6,76 +6,45 @@
 /*   By: jaehyji <jaehyji@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/05 16:03:02 by sejkim2           #+#    #+#             */
-/*   Updated: 2023/10/11 14:17:59 by jaehyji          ###   ########.fr       */
+/*   Updated: 2023/10/11 14:21:04 by jaehyji          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-void run_word(t_tree_node *node)
-{
-	printf("exec %s\n", node->token->value);
-}
-
-void run_redirection_list(t_tree_node *node, t_tree_node **read_redir, t_tree_node **write_redir)
+void run_command(t_tree_node *node, t_fd *p_fd)
 {
 	t_tree_node *child;
+	t_tree_node *redirection_list;
 
 	child = node->child_list;
-	while (child)
-	{
-		//redirection
-		if (child->token->redir_type == SINGLE_REDIR)
-		{
-			if (child->token->value[0] == '<')	//read_redir
-				*read_redir = child;
-			else	//write_redir
-				*write_redir = child;
-		}
-		else	//DOUBLE_REDIR
-		{
-			printf("<< or >>\n");			
-		}
-		child = child->next;	
-	}
+	if (child->symbol == REDIRECTION)
+		run_redirection();	//run_redirection > dup
+	else if (child->symbol == SIMPLE_COMMAND)
+		run_simple_command(child); // simple command에서 elements를 조합
+	else //SUBSHELL
+		run_list();
 }
 
-void run_simple_command_element(t_tree_node *node)
+void run_pipeline(t_tree_node *node, t_fd *p_fd)
 {
 	t_tree_node *child;
+	pid_t	c_pro;
 
-	child = node->child_list;
-	run_word(child);
-}
-
-void run_simple_command(t_tree_node *node)
-{
-	t_tree_node *child;
-	t_tree_node *read_redir;
-	t_tree_node *write_redir;
-
-	child = node->child_list;
-	read_redir = 0;
-	write_redir = 0;
-	while (child)
+	child = node->child_list; //COMMAND
+	if (child->num_of_child > 1)
 	{
-		if (child->symbol == REDIRECTION_LIST)
-			run_redirection_list(child, &read_redir, &write_redir);
-		child = child->next;
+		pipe(p_fd->iput[2]);
+		pipe(p_fd->oput[2]);
 	}
-	child = node->child_list;
-	printf("-------simple_command---------\n");
-	while (child)
+	while ()//next가 null로 도달할 때 까지 단, pipe는 넘김)
 	{
-		if (child->symbol == SIMPLE_COMMAND_ELEMENT)
-			run_simple_command_element(child);
-		child = child->next;
+		c_pro = fork();
+		if (c_pro)
+			run_command(child, p_fd);
+		else
+			child = child->next;
 	}
-	if (read_redir)
-		printf("[read_redir : %s]\n", read_redir->token->value);
-	if (write_redir)
-		printf("[write_redir : %s]\n", write_redir->token->value);
-	printf("-------simple_command end---------\n");
 }
 
 // void run_subshell(t_tree_node *node)
