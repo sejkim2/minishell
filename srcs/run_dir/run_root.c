80e6b29fac409f8fcceb6b2d9daf8033e4e3f6ce
J-millar99 <wlwogus33@gmail.com>
2023-10-25 16:59:17 +0900

    16:58

--- srcs/run_dir/run_root.c
+++ srcs/run_dir/run_root.c
@@ -6,7 +6,7 @@
 /*   By: jaehyji <jaehyji@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/05 16:03:02 by sejkim2           #+#    #+#             */
-/*   Updated: 2023/10/25 16:39:10 by jaehyji          ###   ########.fr       */
+/*   Updated: 2023/10/25 16:59:09 by jaehyji          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,10 +16,6 @@ void	run_root(t_tree_node *root, char ***env)
 {
 	t_tree_node	*child;
 
-	root->fd[0] = dup(0);
-	root->fd[1] = dup(1);
 	child = root->child_list;
 	run_list(child, env, root);
-	close(root->fd[0]);
-	close(root->fd[1]);
 }
