40a597139bf531ff2bcabf6f7389609e025f3f04
j-millar99 <wlwogus33@gmail.com>
2023-10-27 00:08:30 +0900

    signal most stars

--- srcs/signal_dir/heredoc_signal.c
+++ srcs/signal_dir/heredoc_signal.c
@@ -39,3 +39,43 @@ void	set_heredoc_signal(void)
 	signal(SIGINT, SIG_IGN);
 	signal(SIGQUIT, SIG_IGN);
 }
+
+/*
+	void	signal_ctlc(int sig)
+	{
+		if (sig == SIGINT)
+		{
+			write(STDERR_FILENO, "\n", 1);
+			rl_replace_line("", 0);
+			rl_on_new_line();
+			rl_redisplay();
+		}
+	}
+
+	void	signal_ctlc_heredoc(int sig)
+	{
+		if (sig == SIGINT)
+		{
+			close(STDIN_FILENO);
+			write(STDERR_FILENO, "\n", 1);
+		}
+	}
+
+	int	termios_change(bool echo_ctl_chr)
+	{
+		struct termios	terminos_p;
+		int				status;
+
+		status = tcgetattr(STDOUT_FILENO, &terminos_p);
+		if (status == -1)
+			return (ERROR);
+		if (echo_ctl_chr)
+			terminos_p.c_lflag |= ECHOCTL;
+		else
+			terminos_p.c_lflag &= ~(ECHOCTL);
+		status = tcsetattr(STDOUT_FILENO, TCSANOW, &terminos_p);
+		if (status == -1)
+			return (ERROR);
+		return (0);
+	}
+*/
