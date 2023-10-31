8d9fdd04a61e0029836132a8509417d75557afc5
J-millar99 <wlwogus33@gmail.com>
2023-10-31 19:58:40 +0900

    "new master"

--- srcs/builtin_dir/builtin_cd.c
+++ srcs/builtin_dir/builtin_cd.c
@@ -6,15 +6,38 @@
 /*   By: jaehyji <jaehyji@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/12 10:28:08 by jaehyji           #+#    #+#             */
-/*   Updated: 2023/10/28 15:18:33 by jaehyji          ###   ########.fr       */
+/*   Updated: 2023/10/31 19:47:49 by jaehyji          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
+static void	re_path(char **env, char *type, char *path)
+{
+	int		idx;
+	char	*envname;
+	char	*tmp;
+
+	idx = 0;
+	while (env[idx])
+	{
+		envname = ft_strnstr(env[idx], type, ft_strlen(type));
+		if (!envname)
+			idx++;
+		else
+		{
+			free(env[idx]);
+			tmp = ft_strjoin(type, path);
+			env[idx] = tmp;
+			break ;
+		}
+	}
+}
+
 static void	no_argc(char **env)
 {
 	char	*path;
+	char	buff[PATH_MAX];
 
 	path = get_envval("HOME", env);
 	if ((chdir(path) == -1))
@@ -22,27 +45,42 @@ static void	no_argc(char **env)
 		ft_stderror_print("cd", NULL, "HOME not set");
 		g_exit_status = 1;
 	}
+	else
+	{	
+		getcwd(buff, PATH_MAX);
+		re_path(env, "PWD=", buff);
+	}
 	if (path)
 		free(path);
 }
 
-static void	yes_argc(char *path)
+static void	yes_argc(char *path, char **env)
 {
+	char	buff[PATH_MAX];
+
 	if (chdir(path) == -1)
 	{
 		ft_stderror_print("cd", path, strerror(errno));
 		g_exit_status = 1;
+	}
+	else
+	{	
+		getcwd(buff, PATH_MAX);
+		re_path(env, "PWD=", buff);
 	}
 }
 
 void	builtin_cd(char **cmd_argv, char **env)
 {
 	int		cmd_argc;
+	char	buff[PATH_MAX];
 
+	getcwd(buff, PATH_MAX);
+	re_path(env, "OLDPWD=", buff);
 	g_exit_status = 0;
 	cmd_argc = cnt_line(cmd_argv);
 	if (!cmd_argc)
 		no_argc(env);
 	else
-		yes_argc(*cmd_argv);
+		yes_argc(*cmd_argv, env);
 }
